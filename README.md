8784531e26f3d20d590923e1d9f85520f0cc143f
Tom Riat <tom@riat.dev>
2021-07-18 01:21:46 +0200

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# auto-market-trend
-Tool to manage the settings of WickHunter bot depending on the market trend
+# Auto Market Trend
+Auto Market Trend is a tool to manage the settings of WickHunter bot depending on the market trend
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,28 @@
+import java.time.LocalDateTime
+
+enum class Direction {
+    BUY, SELL
+}
+
+enum class Trend {
+    BULLISH, LOWBULLISH, UNCLEAR, LOWBEARISH, BEARISH
+}
+
+class Signal(date: LocalDateTime, direction: Direction) {}
+
+fun initializeSignals() {}
+
+fun marketTrendFromSignals() {}
+
+fun updateSettings(signal: String) {}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var timeInterval: Int = 60 // Time interval of the signals in minutes
+    var signals: List<Signal>?
+    var trend: String?
+    // Init last signals
+
+    // Evaluate trend
+
+    // Update config from trend
 }
